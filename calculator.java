f0c116f08dfc2a9052442d38a90b1775d10173fb
KuaDAR <95322574+KuaDAR@users.noreply.github.com>
2024-02-16 17:08:57 +0800

    Darius Marquez

--- calculator.java
+++ calculator.java
@@ -57,4 +57,20 @@ public class Calculator {
         kb.close();
         System.out.println("\nSum: " + nOne + " + " + nTwo + " = " + (nOne + nTwo));
     }
+      public static void modulus(){
+        int nOne, nTwo;
+        Scanner kb = new Scanner(System.in);
+
+        System.out.println("Modulus");
+
+        System.out.print("\nFirst Number: ");
+        nOne = kb.nextInt();
+
+        System.out.print("\nSecond Number: ");
+        nTwo = kb.nextInt();
+
+        kb.close();
+        System.out.println("\nSum: " + nOne + " % " + nTwo + " = " + (nOne % nTwo));
+    }
+}
 

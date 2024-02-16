d5c66a6a0aaa5afca962421decfd107abfdf3fb4
Ralph Quilling <75477731+hiderim@users.noreply.github.com>
2024-02-16 17:44:00 +0800

    final main

--- calculator.java
+++ calculator.java
@@ -112,16 +112,6 @@ public static void modulus(){
 
         System.out.println("Modulus");
 
-public static void subtraction(){
-
-  
-        public static void subtraction(){
-
-        int nOne, nTwo;
-        Scanner kb = new Scanner(System.in);
-
-        System.out.println("Subtraction");
-
         System.out.print("\nFirst Number: ");
         nOne = kb.nextInt();
 
@@ -129,45 +119,6 @@ public static void subtraction(){
         nTwo = kb.nextInt();
 
         kb.close();
-        System.out.println("\nSum: " + nOne + " - " + nTwo + " = " + (nOne - nTwo));
-    }
-
-    }
-        public static void division(){
-        int nOne, nTwo;
-        Scanner kb = new Scanner(System.in);
-
-        System.out.println("Division");
-          
-        System.out.print("\nFirst Number: ");
-        nOne = kb.nextInt();
-
-        System.out.print("\nSecond Number: ");
-        nTwo = kb.nextInt();
-
-        kb.close();
-
         System.out.println("\nSum: " + nOne + " % " + nTwo + " = " + (nOne % nTwo));
     }
-
-        System.out.println("\nSum: " + nOne + " / " + nTwo + " = " + (nOne / nTwo));
-    }
-
-
-public static void multiplication(){
-    int nOne, nTwo;
-    Scanner kb = new Scanner(System.in);
-
-    System.out.println("Multiplication");
-
-    System.out.print("\nFirst Number: ");
-    nOne = kb.nextInt();
-
-    System.out.print("\nSecond Number: ");
-    nTwo = kb.nextInt();
-
-
-    kb.close();
-    System.out.println("\nSum: " + nOne + " x " + nTwo + " = " + (nOne * nTwo));
-}
 }
